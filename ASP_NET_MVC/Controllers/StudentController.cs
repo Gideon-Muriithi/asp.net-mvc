035e8a8d87620cb32225010d577bc82378d36f94
Gideon Gakenge <ggakenge@CO-OPBANK.CO.KE>
2023-03-22 17:00:15 +0300

    First commit

---
+++ ASP_NET_MVC/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_NET_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            return "How are you doing today sir?";
+        }
+    }
+}
