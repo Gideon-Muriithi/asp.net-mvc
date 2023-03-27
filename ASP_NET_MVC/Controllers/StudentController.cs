6637df179b4e854089c824bac0b9e72bcd1dd073
Gideon Gakenge <ggakenge@CO-OPBANK.CO.KE>
2023-03-27 17:41:36 +0300

    Add MVC

--- ASP_NET_MVC/Controllers/StudentController.cs
+++ ASP_NET_MVC/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP_NET_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,49 @@ namespace ASP_NET_MVC.Controllers
 {
     public class StudentController : Controller
     {
+        IList<Student> studentList = new List<Student> ()
+        {
+            new Student {StudentID = 1,StudentName = "Brian",StudentAge = 18 },
+            new Student {StudentID = 2,StudentName = "Tom",StudentAge = 20 },
+            new Student {StudentID = 3,StudentName = "Ken",StudentAge = 19 }
+        };
         // GET: Student
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
 
-        public string Index()
+        //Action Selectors
+        [NonAction]
+        public string ReturnStr()
         {
             return "How are you doing today sir?";
+        }
+
+        [HttpPost]
+        public ActionResult PostAction()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var std = studentList.Where(s => s.StudentID == Id).FirstOrDefault();
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+
+            //update student in DB using EntityFramework in real-life application
+
+            //update list by removing old student and adding updated student for demo purpose
+            var student = studentList.Where(s => s.StudentID == std.StudentID).FirstOrDefault();
+            studentList.Remove(student);
+            studentList.Add(student);
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ ASP_NET_MVC/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+
+        public int StudentAge { get; set; }
+
+    }
+}
